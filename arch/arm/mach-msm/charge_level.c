b05a726188fec711e5591eb43807de84b564ca1f
anarkia1976 <stefano.villa1976@gmail.com>
2015-04-18 13:56:57 +0200

    fastcharge: qpnp-charger: updated to the latest v1.1
    Thanks a lot
    @Lord Boeffla
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- arch/arm/mach-msm/charge_level.c
+++ arch/arm/mach-msm/charge_level.c
@@ -1,6 +1,8 @@
 /*
- * Author: andip71, 18.09.2014
+ * Author: andip71, 05.11.2014
  *
+ * Version 1.1
+ * 
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -92,10 +94,18 @@ static ssize_t charge_level_usb_store(struct kobject *kobj, struct kobj_attribut
 
 static ssize_t charge_info_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
+	// no charging (both current and requested charge currents are zero)
+	if ((charge_info_level_req == 0) && (charge_info_level_cur == 0))
+		return sprintf(buf, "%s", charge_info_text);
 
-	// print charge info
-	return sprintf(buf, "%s / %d mA", charge_info_text, charge_info_level);
+	// stock charge logic
+	if (charge_level == 0)
+		return sprintf(buf, "%s / %d mA (%d) SL", 
+				charge_info_text, charge_info_level_cur, charge_info_level_req);
 
+	// non-stock charge logic
+		return sprintf(buf, "%s / %d mA (%d)", 
+			charge_info_text, charge_info_level_cur, charge_info_level_req);
 }
 
 
